5743cf32de85ad52c35c4a226b6c713a79be24d8
reshmicoding <reshmicoding@gmail.com>
2026-06-16 10:02:43 +0530

    add gh-pages deploy setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Kimi_Agent_Odissi-Dance-Digital-Book/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
